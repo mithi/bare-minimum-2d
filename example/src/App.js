9a4a71533ed0b7ec7627c6a724c35cebc0c4dbbd
Mithi <mithi.sevilla@gmail.com>
2020-08-07 21:19:29 +0800

    lib: Be able to add data points

--- example/src/App.js
+++ example/src/App.js
@@ -4,13 +4,39 @@ import { BareMinimum2d } from 'bare-minimum-2d'
 import 'bare-minimum-2d/dist/index.css'
 
 const container = {
-  color: '#ff00ff',
-  opacity: 0.5,
+  color: '#0000FF',
+  opacity: 0.2,
+  xRange: 300,
+  yRange: 300
+}
+
+const points1 = {
+  x: [0, -50, -50, 50, 50],
+  y: [0, -50, 50, 50, -50],
+  color: 'red',
+  size: 5,
+  opacity: 0.75,
+  type: 'points',
+  id: 'redColor5'
+}
+
+const points2 = {
+  x: [0, 0, 0, 0],
+  y: [-20, -10, 10, 20],
+  color: 'purple',
+  size: 3,
+  opacity: 0.75,
+  type: 'points',
+  id: 'purple3'
 }
 
 const App = () => {
-  const props = { container }
-  return <div style={{height: "300px", width: "500px"}}><BareMinimum2d {...props} /></div>
+  const props = { container, data: [points1, points2] }
+  return (
+    <div style={{ height: '300px', width: '700px' }}>
+      <BareMinimum2d {...props} />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from 'react'
 // import styles from './styles.module.css'
 
-const Paper = ({ container }) => (
-  <rect
-    width='100%'
-    height='100%'
-    fill={container.color}
-    opacity={container.opacity}
-  />
-)
+let XRANGE = null
+let YRANGE = null
 
-const svgProps = {
-  version: '1.1',
-  baseProfile: 'full',
-  xmlns: 'http://www.w3.org/2000/svg',
-  preserveAspectRatio: 'xMinYMid slice',
-  width: '100%',
-  height: '100%'
-}
-
+// canvasX = x  + xRangeMin
 const transformX = (x, xRange) => x + xRange / 2
+
+// canvasY = yRangeMax - y
 const transformY = (y, yRange) => yRange / 2 - y
 
 /***
@@ -31,23 +19,75 @@ const transformY = (y, yRange) => yRange / 2 - y
                      |
                    -yRange/2
   ***/
+const Paper = ({ container }) => (
+  <rect
+    width='100%'
+    height='100%'
+    fill={container.color}
+    opacity={container.opacity}
+  />
+)
 
-const BareMinimum2d = ({ container }) => {
-  const cx = transformX(-50, container.xRange)
-  const cy = transformY(50, container.yRange)
+const Point = ({ size, color, opacity, id }) => (
+  <circle cx={0} cy={0} r={size} fill={color} opacity={opacity} id={id} />
+)
+const createDefinition = (elementSet) => {
+  if (elementSet.type === 'points') {
+    return <Point {...elementSet} key={elementSet.id} />
+  }
+}
 
+const Definitions = ({ data }) => {
+  const definitions = data.map((elementSet) => createDefinition(elementSet))
+  return <defs>{definitions}</defs>
+}
+
+const svgProps = {
+  version: '1.1',
+  baseProfile: 'full',
+  xmlns: 'http://www.w3.org/2000/svg',
+  preserveAspectRatio: 'xMinYMid slice',
+  width: '100%',
+  height: '100%'
+}
+
+const createElementInstances = (elements) => {
+  return elements.x.map((x, i) => {
+    return (
+      <use
+        x={transformX(x, XRANGE)}
+        y={transformY(elements.y[i], YRANGE)}
+        href={`#${elements.id}`}
+        key={elements.id + i}
+      />
+    )
+  })
+}
+const BareMinimum2d = ({ container, data }) => {
+  XRANGE = container.xRange
+  YRANGE = container.yRange
+  const cx = transformX(20, container.xRange)
+  const cy = transformY(10, container.yRange)
   const view = `0 0 ${container.xRange} ${container.yRange}`
+  const elements = data.reduce((elementsArray, elementSet) => {
+    const elementsSubArray = createElementInstances(elementSet)
+    return [elementsArray, ...elementsSubArray]
+  }, [])
+
   return (
     <svg {...svgProps} viewBox={view}>
       <Paper container={container} />
+      <Definitions data={data} />
       <circle
         cx={cx}
         cy={cy}
-        r='10'
+        r='3'
         stroke='black'
-        strokeWidth='3'
-        fill='red'
+        strokeWidth='2'
+        fill='blue'
       />
+
+      {elements}
     </svg>
   )
 }
