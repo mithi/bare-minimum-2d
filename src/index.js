e514738621b20c7e30c5e7c78ce935a07d6e0244
Mithi <mithi.sevilla@gmail.com>
2020-08-07 19:49:46 +0800

    lib: Add coordinate transform functions and plot range

--- src/index.js
+++ src/index.js
@@ -1,20 +1,53 @@
 import React from 'react'
 // import styles from './styles.module.css'
 
-const Paper = ({ color, opacity }) => (
-  <rect width='100%' height='100%' fill={color} opacity={opacity} />
+const Paper = ({ container }) => (
+  <rect
+    width='100%'
+    height='100%'
+    fill={container.color}
+    opacity={container.opacity}
+  />
 )
 
 const svgProps = {
   version: '1.1',
   baseProfile: 'full',
-  xmlns: 'http://www.w3.org/2000/svg'
+  xmlns: 'http://www.w3.org/2000/svg',
+  preserveAspectRatio: 'xMinYMid slice',
+  width: '100%',
+  height: '100%'
 }
 
+const transformX = (x, xRange) => x + xRange / 2
+const transformY = (y, yRange) => yRange / 2 - y
+
+/***
+                  yRange/2
+                     |
+                     |
+  -xRange/2 -------(0,0)--------- xRange/2
+                     |
+                     |
+                   -yRange/2
+  ***/
+
 const BareMinimum2d = ({ container }) => {
+  const cx = transformX(-50, container.xRange)
+  const cy = transformY(50, container.yRange)
+
+  const view = `0 0 ${container.xRange} ${container.yRange}`
   return (
-    <svg {...svgProps} width='100%' height='100%'>
-      <Paper color={container.color} opacity={container.opacity} />
+    <svg {...svgProps} viewBox={view}>
+      <Paper container={container} />
+      <circle
+        cx={cx}
+        cy={cy}
+        r='10'
+        stroke='black'
+        strokeWidth='3'
+        fill='red'
+      />
     </svg>
   )
 }
