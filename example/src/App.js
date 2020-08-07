41de4a790cdd3d20e33d80cda7ceef38fa12b4a9
Mithi <mithi.sevilla@gmail.com>
2020-08-08 00:34:54 +0800

    lib: Add ability to draw polygons

--- example/src/App.js
+++ example/src/App.js
@@ -14,8 +14,8 @@ const points1 = {
   x: [0, -50, -50, 50, 50],
   y: [0, -50, 50, 50, -50],
   color: 'red',
-  size: 5,
   opacity: 0.75,
+  size: 5,
   type: 'points',
   id: 'redColor5'
 }
@@ -24,8 +24,8 @@ const points2 = {
   x: [0, 0, 0, 0],
   y: [-20, -10, 10, 20],
   color: 'purple',
-  size: 3,
   opacity: 0.75,
+  size: 3,
   type: 'points',
   id: 'purple3'
 }
@@ -36,14 +36,39 @@ const lines1 = {
   x1: [30, -30],
   y1: [20, -50],
   color: '#000000',
-  size: 5,
   opacity: 0.5,
+  size: 5,
   type: 'lines',
   id: 'samplelines'
 }
 
+const lines2 = {
+  x0: [40, 20, 60],
+  y0: [50, 20, -60],
+  x1: [20, -60, -70],
+  y1: [20, -30, 10],
+  color: '#00ff00',
+  opacity: 0.5,
+  size: 10,
+  type: 'lines',
+  id: 'samplelines2'
+}
+
+const [f, m, s] = [40, 70, 30]
+const hexagon = {
+  x: [-f, f, m, f, -f, -m],
+  y: [s, s, 0, -s, -s, 0],
+  fillColor: '#ff00ff',
+  fillOpacity: 0.3,
+  borderColor: '#ff00ff',
+  borderOpacity: 1.0,
+  borderSize: 5,
+  type: 'polygon',
+  id: 'sampleHexagon'
+}
+
 const App = () => {
-  const props = { container, data: [points1, points2, lines1] }
+  const props = { container, data: [points1, points2, lines1, lines2, hexagon] }
   return (
     <div style={{ height: '300px', width: '700px' }}>
       <BareMinimum2d {...props} />
--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ const Points = ({ pointSets }) =>
 /**************************
  * LINES
  **************************/
-const Lines = ({ size, color, opacity, x0, x1, y0, y1, id }) => {
+const LineSet = ({ size, color, opacity, x0, x1, y0, y1, id }) => {
   const d = x0.reduce((currentD, rawX0, i) => {
     const [currentX0, currentX1, currentY0, currentY1] = [
       transformX(rawX0, XRANGE),
@@ -82,26 +82,71 @@ const Lines = ({ size, color, opacity, x0, x1, y0, y1, id }) => {
     return `${currentD} M ${currentX0},${currentY0} L ${currentX1},${currentY1} `
   }, '')
 
-  console.log('path', d)
   return (
     <path d={d} stroke={color} strokeWidth={size} opacity={opacity} id={id} />
   )
 }
+
+const Lines = ({ lineSets }) =>
+  lineSets.map((lineSet) => <LineSet {...lineSet} key={lineSet.id} />)
+
+/**************************
+ * Polygon
+ **************************/
+
+const Polygon = ({
+  x,
+  y,
+  fillColor,
+  fillOpacity,
+  borderColor,
+  borderOpacity,
+  borderSize,
+  id
+}) => {
+  const pointString = x.reduce((pointString, rawX, i) => {
+    const currentX = transformX(rawX, XRANGE)
+    const currentY = transformY(y[i], YRANGE)
+    return `${pointString}${currentX},${currentY} `
+  }, '')
+
+  return (
+    <polygon
+      points={pointString}
+      fill={fillColor}
+      fillOpacity={fillOpacity}
+      stroke={borderColor}
+      strokeWidth={borderSize}
+      strokeOpacity={borderOpacity}
+      id={id}
+    />
+  )
+}
+const Polygons = ({ polygonSets }) =>
+  polygonSets.map((polygon) => <Polygon {...polygon} key={polygon.id} />)
+/**************************
+ * Minimal Plot
+ **************************/
+
+const filterSet = (data, type) =>
+  data.filter((dataSet) => dataSet.type === type)
 
 const BareMinimum2d = ({ container, data }) => {
   XRANGE = container.xRange
   YRANGE = container.yRange
   const view = `0 0 ${container.xRange} ${container.yRange}`
 
-  const pointSets = data.filter((elementSet) => elementSet.type === 'points')
-  const lineSets = data.filter((elementSet) => elementSet.type === 'lines')
-  const props = lineSets[0]
+  const pointSets = filterSet(data, 'points')
+  const lineSets = filterSet(data, 'lines')
+  const polygonSets = filterSet(data, 'polygon')
+
   return (
     <svg {...svgProps} viewBox={view}>
       <Paper container={container} />
       <PointDefinitions pointSets={pointSets} />
+      <Polygons polygonSets={polygonSets} />
+      <Lines lineSets={lineSets} />
       <Points pointSets={pointSets} />
-      <Lines {...props} />
     </svg>
   )
 }
