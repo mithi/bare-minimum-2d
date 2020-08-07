d35fc8dd8e162fc66b2f91970f77f52d685fe620
Mithi <mithi.sevilla@gmail.com>
2020-08-07 18:49:28 +0800

    lib: Do various style changes - Refactor - Remove unused elements - The view port or canvas size of the svg elements take the size of the container

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,14 @@ import React from 'react'
 import { BareMinimum2d } from 'bare-minimum-2d'
 import 'bare-minimum-2d/dist/index.css'
 
+const container = {
+  color: '#ff00ff',
+  opacity: 0.5,
+}
+
 const App = () => {
-  return <BareMinimum2d color='#00ff00' />
+  const props = { container }
+  return <div style={{height: "300px", width: "500px"}}><BareMinimum2d {...props} /></div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 // import styles from './styles.module.css'
 
-const BareMinimum2d = (props) => {
+const Paper = ({ color, opacity }) => (
+  <rect width='100%' height='100%' fill={color} opacity={opacity} />
+)
+
+const svgProps = {
+  version: '1.1',
+  baseProfile: 'full',
+  xmlns: 'http://www.w3.org/2000/svg'
+}
+
+const BareMinimum2d = ({ container }) => {
   return (
-    <svg
-      version='1.1'
-      baseProfile='full'
-      width={1000}
-      height={500}
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <rect width='100%' height='100%' fill={props.color} opacity={0.6} />
-      <circle cx={150} cy={100} r='80' fill='#00ffff' />
-      <text x={150} y={125} fontSize='50' textAnchor='middle' fill='#ff00ff'>
-        Hello World!
-      </text>
+    <svg {...svgProps} width='100%' height='100%'>
+      <Paper color={container.color} opacity={container.opacity} />
     </svg>
   )
 }
