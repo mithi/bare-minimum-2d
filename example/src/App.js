c1815a9878ea6e97bb3498e4ca9ea01c06200742
Mithi <mithi.sevilla@gmail.com>
2020-08-09 19:54:06 +0800

    demo: Add routing to pages

--- example/src/App.js
+++ example/src/App.js
@@ -2,22 +2,44 @@ import React from 'react'
 import Demo3 from './demo3/demo'
 import Demo2 from './demo2/demo'
 import Demo1 from './demo1/demo'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
+
+const stickyHome = {
+  position: 'fixed',
+  top: 0,
+  margin: '10px',
+  color: '#32ff7e'
+}
+
+const LandingPage = () => (
+  <div style={{ marginTop: '30px', marginLeft: '10px' }}>
+    <Link to='/demo1'>Demo1</Link>
+    <br />
+    <Link to='/demo2'>Demo2</Link>
+    <br />
+    <Link to='/demo3'>Demo3</Link>
+    <br />
+  </div>
+)
 
 const App = () => {
   return (
     <Router>
       <div>
+        <div style={stickyHome}>
+          <Link to='/'>Home</Link>
+        </div>
+
         <Switch>
           <Route exact path='/'>
-            <Link to='/demo1'>Demo1</Link>
-            <br />
-            <Link to='/demo2'>Demo2</Link>
-            <br />
-            <Link to='/demo3'>Demo3</Link>
-            <br />
+            <LandingPage />
           </Route>
-
           <Route exact path='/demo1'>
             <Demo1 />
           </Route>
@@ -26,6 +48,9 @@ const App = () => {
           </Route>
           <Route exact path='/demo3'>
             <Demo3 />
+          </Route>
+          <Route>
+            <Redirect to='/' />
           </Route>
         </Switch>
       </div>
