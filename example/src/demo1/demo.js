27b3b081c47ce838ca21259552f42ab84afddce2
Mithi <mithi.sevilla@gmail.com>
2020-08-09 16:22:27 +0800

    demo: Refactor demo1 and add description

--- example/src/demo1/demo.js
+++ example/src/demo1/demo.js
@@ -3,13 +3,46 @@ import { ResizeObserver } from '@juggle/resize-observer'
 import { BareMinimum2d } from 'bare-minimum-2d'
 import DEMO_PROPS from './demoProps'
 
+/*****
+ DEMO #1
+
+ This demo shows that the BareMinimum2d component
+ takes the dimensions of its parent component
+
+ It also illustrates the data structure of the passed props
+ as props to BareMinimum2d and the resulting svg which is
+ be pretty expressive
+
+ Checkout demoProps.js and demo.svg located at this same directory
+
+ This component which wraps < BareMinimum2d /> uses ResizeObserver
+ to listen for changes in dimensions of its width
+ (to display it to the user) when changed, whenever this occurs
+ we take this opportunity to get the document window's height
+ and sync it to the height of the component.
+
+ *****/
 const STICKY_DIV_STYLE = {
   position: 'fixed',
   top: 0,
   fontSize: 10,
-  color: '#32ff7e'
+  color: '#32ff7e',
+  paddingLeft: '5px'
 }
-class DemoOne extends React.PureComponent {
+
+const DemoSticky = ({ height, width }) => (
+  <div style={STICKY_DIV_STYLE}>
+    [Go back]
+    <br />
+    Resize the window.
+    <br />
+    BareMinimum2d takes the dimensions of its parent.
+    <br />
+    {height} x {width}
+  </div>
+)
+
+class Demo extends React.PureComponent {
   resizeObserver = null
   element = React.createRef()
   width = '100%'
@@ -20,14 +53,12 @@ class DemoOne extends React.PureComponent {
 
   componentDidMount() {
     this.resizeObserver = new ResizeObserver((entries, _) => {
-      for (let entry of entries) {
-        const { inlineSize } = entry.contentBoxSize
-        const { width } = entry.contentRect
-        this.setState({
-          width: inlineSize || width,
-          height: window.innerHeight
-        })
-      }
+      const { inlineSize } = entries[0].contentBoxSize
+      const { width } = entries[0].contentRect
+      this.setState({
+        height: window.innerHeight
+      })
+      this.width = inlineSize || width
     })
     this.resizeObserver.observe(this.element.current)
   }
@@ -37,21 +68,18 @@ class DemoOne extends React.PureComponent {
   }
 
   render() {
-    const height = this.state.height
-    const containerStyle = { height, width: this.width }
+    const { height } = this.state
 
     return (
-      <div ref={this.element} style={containerStyle}>
+      <div ref={this.element} style={{ height, width: '100%' }}>
         <BareMinimum2d
           data={DEMO_PROPS.data}
           container={DEMO_PROPS.container}
         />
-        <div style={STICKY_DIV_STYLE}>
-          {height}x{this.state.width}
-        </div>
+        <DemoSticky {...{ height, width: this.width }} />
       </div>
     )
   }
 }
 
-export default DemoOne
+export default Demo
