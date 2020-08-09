93a20012b60e8c1d6be75d3742f4ace556476779
Mithi <mithi.sevilla@gmail.com>
2020-08-09 19:03:33 +0800

    demo: Add preliminary routing

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import DemoOne from './demo1'
+import Demo3 from './demo3/demo'
+import Demo2 from './demo2/demo'
+import Demo1 from './demo1/demo'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <DemoOne />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+            <Link to='/demo1'>Demo1</Link>
+            <br />
+            <Link to='/demo2'>Demo2</Link>
+            <br />
+            <Link to='/demo3'>Demo3</Link>
+            <br />
+          </Route>
+
+          <Route exact path='/demo1'>
+            <Demo1 />
+          </Route>
+          <Route exact path='/demo2'>
+            <Demo2 />
+          </Route>
+          <Route exact path='/demo3'>
+            <Demo3 />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
